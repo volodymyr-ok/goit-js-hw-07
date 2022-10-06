7869fa197143fd59dcd8d842eccddb284109a2dd
Volodymyr Ok <v.shark.t@gmail.com>
2022-10-06 19:56:39 +0300

    done hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const getEl = (selector) => document.querySelector(selector);
+const galleryEl = getEl(".gallery");
 
-console.log(galleryItems);
+const galleryImages = galleryItems
+  .map(({ original, preview, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  })
+  .join(" ");
+galleryEl.insertAdjacentHTML("beforeend", galleryImages);
+
+galleryEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const {
+    nodeName,
+    dataset: { source },
+  } = event.target;
+
+  if (nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+`);
+
+  instance.show();
+});
+
+galleryEl.addEventListener("keydown", ({ key }) => {
+  if (key !== "Escape") {
+    return;
+  }
+  getEl(".basicLightbox").remove(".basicLightbox--visible");
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const getEl = selector => document.querySelector(selector);
+const galleryEl = getEl('.gallery');
 
-console.log(galleryItems);
+const galleryImages = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`,
+  )
+  .join(' ');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryImages);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
